a4b04f6c8f46d142122493c63231e9e4632345e1
imehtadel <imehta.del@gmail.com>
2026-06-23 02:41:38 +0100

    Fixed Issues

--- src/main/java/com/awin/interview/transactionapi/exception/InvalidRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.awin.interview.transactionapi.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class InvalidRequestException extends RuntimeException {
-    public InvalidRequestException(HttpStatus httpStatus, String message) {
-        super(message);
-    }
-}
